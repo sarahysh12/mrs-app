8b166a1d4f05ceea06b3da26b47da624243de56e
sarahysh12 <saraysh22@gmail.com>
2021-01-11 21:24:45 -0800

    complete the game

--- App.js
+++ App.js
@@ -2,28 +2,37 @@ import React, { useState } from 'react';
 import {
   StyleSheet,
   View,
-  Button,
-  FlatList
 } from 'react-native';
 
-// import GoalItem from './components/GoalItem';
-// import GoalInput from './components/GoalInput';
 import Header from './components/Header';
+import GameOverScreen from './screens/GameOverScreen';
 import GameScreen from './screens/GameScreen';
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
-    
     const [userNumber, setUserNumber] = useState();
+    const [guessRounds, setGuessRounds] = useState(0);
+
+    const configureNewGameHandler = () => {
+      setGuessRounds(0);
+      setUserNumber(null);
+    }
 
     const startGameHandler = (selectedNumber) => {
       setUserNumber(selectedNumber);
+      setGuessRounds(0);
+    };
+
+    const gameOverHandler = (numOfRounds) => {
+      setGuessRounds(numOfRounds);
     };
 
     let content = <StartGameScreen onStartGame={startGameHandler}/>;
 
-    if(userNumber) {
-      content = <GameScreen userChoice={userNumber}/>;
+    if(userNumber && guessRounds <= 0) {
+      content = <GameScreen userChoice={userNumber} onGameOver={gameOverHandler}/>;
+    } else if (guessRounds > 0) {
+      content = <GameOverScreen roundsNumber={guessRounds} userNumber={userNumber} onRestart={configureNewGameHandler}/>
     }
     
     return (
