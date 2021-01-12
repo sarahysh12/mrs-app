be90a5dd48299c92f99c6dc76c09ac838b57f9e5
sarahysh12 <saraysh22@gmail.com>
2021-01-11 22:51:09 -0800

    more styles

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import Colors from '../constants/colors';
+import BodyText from '../components/BodyText';
+import TitleText from '../components/TitleText';
 
 const GameOverScreen = props => {
     return (
     <View style={styles.screen}>
-        <Text>The Game Is Over!</Text>
-        <Text>Number of rounds: {props.roundsNumber}</Text>
-        <Text>Number was: {props.userNumber}</Text>
+        <TitleText>The Game Is Over!</TitleText>
+        <View style={styles.imageContainer}>
+            <Image source={require('../assets/success.png')} style={styles.image} resizeMode="cover"/>
+            {/* <Image fadeDuration={1000} source={{uri: 'https://abrahamswallet.com/wp-content/uploads/2017/12/samuel-ferrara-117219-1180x770.jpg'}}/> */}
+        </View>
+        <View style={styles.resultContainer}>
+            <BodyText style={styles.resultText}>
+                Your phone needed {' '}
+                <Text style={styles.highlight}>{props.userNumber}</Text> 
+                rounds to guess the number {' '}
+                <Text style={styles.highlight}>{props.roundsNumber}</Text>
+            </BodyText>
+        </View>
         <Button title="NEW GAME" onPress={() => {props.onRestart}}/>
     </View>
     );
@@ -17,6 +30,31 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    image: {
+        width: '100%',
+        height: '100%'
+    },
+    imageContainer: {
+        height: 300,
+        width: 300,
+        borderRadius: 150,
+        borderWidth: 3,
+        borderColor: 'black',
+        overflow: 'hidden',
+        marginVertical: 30
+    },
+    resultText: {
+        textAlign: 'center',
+        fontSize: 20
+    },
+    highlight: {
+        color: Colors.primary,
+        fontFamily: 'open-sans-bold'
+    },
+    resultContainer: {
+       marginHorizontal: 30,
+       marginVertical: 15
     }
 });
 
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -12,6 +12,8 @@ import Card from '../components/Card';
 import Colors from '../constants/colors';
 import Input from '../components/Input';
 import NumberContainer from '../components/NumberContainer';
+import BodyText from '../components/BodyText';
+import TitleText from '../components/TitleText';
 
 const StartGameScreen = props => {
     const [enteredValue, setEnteredValue] = useState('');
@@ -24,7 +26,6 @@ const StartGameScreen = props => {
     };
 
     const resetInputHandler = () => {
-        console.log('fuck');
         setEnteredValue('');
         setConfirmed(false);
     };
@@ -58,9 +59,9 @@ const StartGameScreen = props => {
     return (
         <TouchableWithoutFeedback onPress={()=> {Keyboard.dismiss();}}>
             <View style={styles.screen}>
-                <Text style={styles.title}>Start a New Game</Text>
+                <TitleText style={styles.title}>Start a New Game</TitleText>
                 <Card style={styles.inputContainer}>
-                    <Text>Select a Number</Text>
+                    <BodyText>Select a Number</BodyText>
                     <Input 
                         style={styles.input} 
                         blurOnSubmit 
@@ -90,6 +91,7 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 20,
         marginVertical: 10,
+        fontFamily: 'open-sans-bold'
     },
     buttonContainer: {
         flexDirection: 'row',
